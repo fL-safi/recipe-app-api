a3e15f33310dea6a4b4318293aa94c77412cc6f5
Safi-forsit <safiullah@forsit.co.kr>
2024-08-01 12:24:30 +0500

    recipe tags and ingredients
    recipe tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,7 +9,8 @@ from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
     Recipe,
-    Tag
+    Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -44,6 +45,20 @@ class TagViewSet(mixins.DestroyModelMixin, mixins.UpdateModelMixin, mixins.ListM
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+class IngredientViewSet(mixins.DestroyModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredient in database"""
+
+
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
     def get_queryset(self):
         """Filter queryset for authenticated user"""
